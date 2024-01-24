e83b17149183ea97d794026e3ee7c245deda59c1
ThadshajinyK <kaneshanthadshajiny@gmail.com>
2024-01-24 10:54:41 +0530

    Set up backend project and add dependencies

---
+++ src/main/java/com/example/RiderBackend/RiderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.RiderBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RiderBackend/RiderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RiderBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
